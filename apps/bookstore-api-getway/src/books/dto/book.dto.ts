b2f521de08f5f200c0fd8b7cb028132a5092a326
Mohammad Saeed Dalil <mohammad.saeed.dalil@lit-co.net>
2024-10-28 16:41:19 +0300

    Complete CRUD Using Way 1

---
+++ apps/bookstore-api-getway/src/books/dto/book.dto.ts
@@ -0,0 +1,6 @@
+export class BookDtos {
+    id: number;
+    title: string;
+    author: string;
+    rating: number;
+}
--- apps/bookstore-api-getway/src/bookstore-api-getway.module.ts
+++ apps/bookstore-api-getway/src/bookstore-api-getway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookstoreApiGetwayController } from './bookstore-api-getway.controller';
 import { BookstoreApiGetwayService } from './bookstore-api-getway.service';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule],
   controllers: [BookstoreApiGetwayController],
   providers: [BookstoreApiGetwayService],
 })
